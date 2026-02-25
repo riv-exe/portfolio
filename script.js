39347dec93ca809a2093fd07596994470752ac4a
Paulo <pauloreeve143@gmail.com>
2026-02-25 21:24:48 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuIcon = document.getElementById('setting-icon');
+const navbar = document.getElementById('navbar');
+const notDone = document.querySelectorAll('.not-done');
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('show');
+});
+
+notDone.forEach(link => {
+    link.addEventListener('click', () => {
+        alert('Not yet done! First page only.');
+    })
+})
